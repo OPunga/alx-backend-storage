72baad6fcf6a98ff50cc189c9715546e8beb1924
OPunga <ronnyopunga96@gmail.com>
2023-01-13 00:21:17 +0300

    table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,7 @@
-BEGIN
-  IF NOT EXISTS (SELECT * FROM information_schema.tables WHERE table_name = 'users') THEN
-    CREATE TABLE users (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      email VARCHAR(255) NOT NULL UNIQUE,
-      name VARCHAR(255) NOT NULL
-    );
-  END IF;
-END;
+-- Creat user table
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+);
